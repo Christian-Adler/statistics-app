cc7abe1c86dcfeeab00a04468b6d61f3e8ac219a
Christian Adler <chris.adler@gmx.de>
2023-02-13 20:45:32 +0100

    add dummy chart

--- lib/screens/chart_screen.dart
+++ lib/screens/chart_screen.dart
@@ -1,5 +1,8 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:statistics/widgets/StatisticsAppBar.dart';
+import 'package:statistics/widgets/app_drawer.dart';
+
+import '../widgets/statistics_app_bar.dart';
 
 class ChartScreen extends StatelessWidget {
   static const String routeName = '/chart_screen';
@@ -10,7 +13,7 @@ class ChartScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: StatisticsAppBar(
-        'Charts',
+        const Text('Charts'),
         context,
         actions: [
           IconButton(
@@ -19,7 +22,54 @@ class ChartScreen extends StatelessWidget {
           )
         ],
       ),
-      body: const Center(child: Text('test')),
+      // appBar: AppBar(),
+
+      // appBar: GradientAppBar(title: Text('abc'), gradientColors: [Colors.black12, Colors.red]),
+
+      drawer: const AppDrawer(),
+      body: AspectRatio(
+        aspectRatio: 1.5,
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: LineChart(
+            LineChartData(
+              minY: -1,
+              maxY: 1,
+              minX: 0,
+              maxX: 10,
+              lineTouchData: LineTouchData(enabled: false),
+              clipData: FlClipData.all(),
+              gridData: FlGridData(
+                show: true,
+                drawVerticalLine: false,
+              ),
+              borderData: FlBorderData(show: false),
+              lineBarsData: [
+                LineChartBarData(
+                  spots: [
+                    FlSpot(1, 0.5),
+                    FlSpot(2, 0.7),
+                    FlSpot(3, 0.2),
+                    FlSpot(4, 0.5),
+                  ],
+                  dotData: FlDotData(
+                    show: false,
+                  ),
+                  gradient: LinearGradient(
+                    colors: [Colors.blueAccent, Colors.purple],
+                    stops: const [0.1, 1.0],
+                  ),
+                  barWidth: 4,
+                  isCurved: false,
+                ),
+              ],
+              titlesData: FlTitlesData(
+                show: false,
+              ),
+            ),
+          ),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           print('Pressed');
