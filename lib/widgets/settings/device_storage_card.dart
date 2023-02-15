45093cf4dcbd05c06cd42e878faa70dab6b39937
Christian Adler <chris.adler@gmx.de>
2023-02-15 05:35:17 +0100

    splitt device storage card into multiple widgets

--- lib/widgets/settings/device_storage_card.dart
+++ lib/widgets/settings/device_storage_card.dart
@@ -1,36 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:statistics/models/device_storage.dart';
 
 import '../../models/globals.dart';
-import '../../providers/auth.dart';
 
-class DeviceStorageCard extends StatefulWidget {
+class DeviceStorageCard extends StatelessWidget {
   const DeviceStorageCard({Key? key}) : super(key: key);
 
   @override
-  State<DeviceStorageCard> createState() => _DeviceStorageCardState();
-}
-
-class _DeviceStorageCardState extends State<DeviceStorageCard> {
-  var _showDeviceStorage = false;
-
-  void _toggleShowDeviceStorage() {
-    setState(() {
-      _showDeviceStorage = !_showDeviceStorage;
-    });
-  }
-
-  void _hideShowDeviceStorage() {
-    setState(() {
-      _showDeviceStorage = false;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final auth = Provider.of<Auth>(context, listen: false);
-
     return Card(
       child: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -41,61 +18,76 @@ class _DeviceStorageCardState extends State<DeviceStorageCard> {
               'Device Storage',
               style: Theme.of(context).textTheme.titleLarge,
             ),
-            OutlinedButton.icon(
-              onPressed: _toggleShowDeviceStorage,
-              icon: Icon(_showDeviceStorage ? Icons.visibility_off_rounded : Icons.visibility_rounded),
-              label: Text(_showDeviceStorage ? 'hide storage' : 'show storage'),
-            ),
-            if (_showDeviceStorage)
-              FutureBuilder(
-                builder: (context, deviceStorageSnapshot) {
-                  if (deviceStorageSnapshot.connectionState == ConnectionState.waiting) {
-                    return const LinearProgressIndicator();
-                  }
-
-                  final storageData = deviceStorageSnapshot.data;
-                  if (storageData == null) return const Text('No device storage data set.');
-
-                  List<TableRow> rows = [_tableHeadline('Key', 'Value')];
-
-                  final keys = storageData.keys.toList();
-                  keys.sort();
-                  for (var key in keys) {
-                    final value = storageData[key];
-                    rows.add(_tableRow(key, value ?? '-'));
-                  }
-
-                  return Table(
-                    // https://api.flutter.dev/flutter/widgets/Table-class.html
-                    columnWidths: const <int, TableColumnWidth>{
-                      0: FixedColumnWidth(128), // IntrinsicColumnWidth(),
-                      1: FlexColumnWidth(),
-                    },
-                    border: TableBorder.symmetric(
-                      inside: const BorderSide(width: 1, color: Colors.black12),
-                    ),
-                    children: rows,
-                  );
-                },
-                future: DeviceStorage.readAll(),
-              ),
+            const _ShowDeviceStorage(),
             const Divider(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                OutlinedButton.icon(
-                  onPressed: () async {
-                    await Globals.logout(context);
-                    await DeviceStorage.deleteAll();
-                  },
-                  icon: const Icon(Icons.lock_reset),
-                  label: const Text('clear storage & logout'),
-                ),
-              ],
-            ),
+            const _ClearDeviceStorage(),
           ],
         ),
       ),
+    );
+  }
+}
+
+class _ShowDeviceStorage extends StatefulWidget {
+  const _ShowDeviceStorage();
+
+  @override
+  State<_ShowDeviceStorage> createState() => _ShowDeviceStorageState();
+}
+
+class _ShowDeviceStorageState extends State<_ShowDeviceStorage> {
+  var _showDeviceStorage = false;
+
+  void _toggleShowDeviceStorage() {
+    setState(() {
+      _showDeviceStorage = !_showDeviceStorage;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        OutlinedButton.icon(
+          onPressed: _toggleShowDeviceStorage,
+          icon: Icon(_showDeviceStorage ? Icons.visibility_off_rounded : Icons.visibility_rounded),
+          label: Text(_showDeviceStorage ? 'hide storage' : 'show storage'),
+        ),
+        if (_showDeviceStorage)
+          FutureBuilder(
+            builder: (context, deviceStorageSnapshot) {
+              if (deviceStorageSnapshot.connectionState == ConnectionState.waiting) {
+                return const LinearProgressIndicator();
+              }
+
+              final storageData = deviceStorageSnapshot.data;
+              if (storageData == null) return const Text('No device storage data set.');
+
+              List<TableRow> rows = [_tableHeadline('Key', 'Value')];
+
+              final keys = storageData.keys.toList();
+              keys.sort();
+              for (var key in keys) {
+                final value = storageData[key];
+                rows.add(_tableRow(key, value ?? '-'));
+              }
+
+              return Table(
+                // https://api.flutter.dev/flutter/widgets/Table-class.html
+                columnWidths: const <int, TableColumnWidth>{
+                  0: FixedColumnWidth(128), // IntrinsicColumnWidth(),
+                  1: FlexColumnWidth(),
+                },
+                border: TableBorder.symmetric(
+                  inside: const BorderSide(width: 1, color: Colors.black12),
+                ),
+                children: rows,
+              );
+            },
+            future: DeviceStorage.readAll(),
+          ),
+      ],
     );
   }
 
@@ -133,5 +125,47 @@ class _DeviceStorageCardState extends State<DeviceStorageCard> {
         child: Text(value),
       )
     ]);
+  }
+}
+
+class _ClearDeviceStorage extends StatelessWidget {
+  const _ClearDeviceStorage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        OutlinedButton.icon(
+          onPressed: () async {
+            showDialog(
+              context: context,
+              builder: (ctx) => AlertDialog(
+                title: const Text('Are you sure?'),
+                content: const Text('Do you really want to remove all data from device and log out?'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(ctx).pop(false);
+                    },
+                    child: const Text('No'),
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      Navigator.of(ctx).pop(true);
+                      await Globals.logout(ctx);
+                      await DeviceStorage.deleteAll();
+                    },
+                    child: const Text('Yes'),
+                  ),
+                ],
+              ),
+            );
+          },
+          icon: const Icon(Icons.lock_reset),
+          label: const Text('clear storage & logout'),
+        ),
+      ],
+    );
   }
 }
