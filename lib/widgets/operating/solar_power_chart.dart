cbb82eda92c4722a2a3501c95926a65db71cb95c
Christian Adler <chris.adler@gmx.de>
2023-02-17 04:35:43 +0100

    add second line in chart

--- lib/widgets/operating/solar_power_chart.dart
+++ lib/widgets/operating/solar_power_chart.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -18,11 +20,16 @@ class SolarPowerChart extends StatelessWidget {
     final chartMeta = powerData.solarPowerChartMetaData;
 
     final List<FlSpot> spots = [];
+    final List<FlSpot> spots2 = [];
+    var toggle = false;
     for (var item in powerData.solarPowerItems) {
       spots.add(FlSpot(item.xValue, item.value));
+      spots2.add(FlSpot(item.xValue, math.sqrt(item.value)));
+      toggle = !toggle;
     }
 
     final gradientColors = [colorScheme.primary, colorScheme.secondary];
+    final gradientColors2 = [Colors.redAccent, Colors.deepPurpleAccent.withOpacity(0.1)];
 
     return Column(children: [
       Padding(
@@ -33,6 +40,8 @@ class SolarPowerChart extends StatelessWidget {
       ),
       Charts.createChartContainer(
           Charts.createLineChartMonthlyData(chartMeta, [
+            Charts.createLineChartBarData(spots2, gradientColors2,
+                shadow: false, fillColors: gradientColors2.map((color) => color.withOpacity(0.5)).toList()),
             Charts.createLineChartBarData(spots, gradientColors),
           ]),
           orientation),
