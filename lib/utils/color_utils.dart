08c30757f9ae9e2155b831da37aad3c34363c927
Christian Adler <chris.adler@gmx.de>
2023-02-25 21:37:12 +0100

    add ColorUtils

---
+++ lib/utils/color_utils.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ColorUtils {
+  // https://stackoverflow.com/questions/58360989/programmatically-lighten-or-darken-a-hex-color-in-dart
+
+  /// Darken a color by [percent] amount (100 = black)
+// ........................................................
+  static Color darken(Color c, [int percent = 10]) {
+    assert(1 <= percent && percent <= 100);
+    var f = 1 - percent / 100;
+    return Color.fromARGB(c.alpha, (c.red * f).round(), (c.green * f).round(), (c.blue * f).round());
+  }
+
+  /// Lighten a color by [percent] amount (100 = white)
+// ........................................................
+  static Color lighten(Color c, [int percent = 10]) {
+    assert(1 <= percent && percent <= 100);
+    var p = percent / 100;
+    return Color.fromARGB(c.alpha, c.red + ((255 - c.red) * p).round(), c.green + ((255 - c.green) * p).round(),
+        c.blue + ((255 - c.blue) * p).round());
+  }
+
+  static Color hue(Color c, [double add = 10]) {
+    var hsl = HSLColor.fromColor(c);
+    return hsl.withHue(hsl.hue + add).toColor();
+  }
+}
