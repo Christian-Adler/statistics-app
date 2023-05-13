5233a17723ba08cd86ba7e37a9821711f3a83f7a
Christian Adler <chris.adler@gmx.de>
2023-05-13 09:06:54 +0200

    refactor navigation items
    named items
    navigation per IndexedStack

--- lib/models/navigation/navigation_item.dart
+++ lib/models/navigation/navigation_item.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:statistics/models/navigation/navigation_item_base.dart';
 import 'package:statistics/models/navigation/navigation_item_type.dart';
 
+import '../../providers/main_navigation.dart';
 import 'screen_nav_info.dart';
 
 class NavigationItem extends NavigationItemBase {
   ScreenNavInfo screenNavInfo;
-  final void Function(BuildContext context, NavigatorState navigator) onNav;
 
-  NavigationItem(this.screenNavInfo, this.onNav) : super(NavigationItemType.navigation);
+  final void Function(BuildContext context)? onNavOverride;
+
+  NavigationItem(this.screenNavInfo, {this.onNavOverride}) : super(NavigationItemType.navigation);
 
   IconData get iconData => screenNavInfo.iconData;
 
   String get title => screenNavInfo.title;
+
+  void onNav(BuildContext context) {
+    var navOverride = onNavOverride;
+    if (navOverride != null) {
+      navOverride(context);
+      return;
+    }
+    Provider.of<MainNavigation>(context, listen: false).mainPageRoute = screenNavInfo.routeName;
+  }
 }
--- lib/models/navigation/navigation_items.dart
+++ lib/models/navigation/navigation_items.dart
@@ -8,7 +8,6 @@ import '../../screens/operating/solar_power_screen.dart';
 import '../../screens/overview_screen.dart';
 import '../../screens/settings_screen.dart';
 import '../../utils/globals.dart';
-import '../../utils/nav/navigation_utils.dart';
 import 'navigation_divider_large.dart';
 import 'navigation_divider_small.dart';
 import 'navigation_item.dart';
@@ -16,79 +15,66 @@ import 'navigation_item_base.dart';
 import 'screen_nav_info.dart';
 
 class NavigationItems {
+  static final NavigationItem _logoutNavigationItem = NavigationItem(
+    const ScreenNavInfo('Logout', Icons.exit_to_app, '/logout'),
+    onNavOverride: (context) => Globals.logout(context),
+  );
+
+  static final NavigationItem _overviewNavigationItem = NavigationItem(OverviewScreen.screenNavInfo);
+  static final NavigationItem _operatingNavigationItem = NavigationItem(
+    OperatingScreen.screenNavInfo,
+  );
+  static final NavigationItem _solarPowerNavigationItem = NavigationItem(
+    SolarPowerScreen.screenNavInfo,
+  );
+  static final NavigationItem _carNavigationItem = NavigationItem(
+    CarScreen.screenNavInfo,
+  );
+  static final NavigationItem _heartNavigationItem = NavigationItem(
+    HeartScreen.screenNavInfo,
+  );
+  static final NavigationItem _settingsNavigationItem = NavigationItem(
+    SettingsScreen.screenNavInfo,
+  );
+  static final NavigationItem _infoNavigationItem = NavigationItem(
+    InfoScreen.screenNavInfo,
+  );
+
+  /// indexedStackElemente
+  static final List<NavigationItem> mainNavigationItems = [
+    _overviewNavigationItem,
+    _operatingNavigationItem,
+    _solarPowerNavigationItem,
+    _carNavigationItem,
+    _heartNavigationItem,
+    _settingsNavigationItem,
+    _infoNavigationItem,
+  ];
+
   static final List<NavigationItemBase> navigationMenuItems = [
-    NavigationItem(
-        OverviewScreen.screenNavInfo, (context, navigator) => NavigationUtils.navigateToRoute(context, navigator, [])),
+    _overviewNavigationItem,
     NavigationDividerSmall(),
-    NavigationItem(
-        OperatingScreen.screenNavInfo,
-        (context, navigator) =>
-            NavigationUtils.navigateToRoute(context, navigator, [OperatingScreen.screenNavInfo.routeName])),
-    // NavigationDividerSmall(),
-    NavigationItem(
-        SolarPowerScreen.screenNavInfo,
-        (context, navigator) =>
-            NavigationUtils.navigateToRoute(context, navigator, [SolarPowerScreen.screenNavInfo.routeName])),
-    // NavigationDividerSmall(),
-    NavigationItem(
-        CarScreen.screenNavInfo,
-        (context, navigator) =>
-            NavigationUtils.navigateToRoute(context, navigator, [CarScreen.screenNavInfo.routeName])),
-    // NavigationDividerSmall(),
-    NavigationItem(
-        HeartScreen.screenNavInfo,
-        (context, navigator) =>
-            NavigationUtils.navigateToRoute(context, navigator, [HeartScreen.screenNavInfo.routeName])),
-    // NavigationDividerLarge(),
+    _operatingNavigationItem,
+    _solarPowerNavigationItem,
+    _carNavigationItem,
+    _heartNavigationItem,
     NavigationDividerSmall(),
-    NavigationItem(
-        SettingsScreen.screenNavInfo,
-        (context, navigator) =>
-            NavigationUtils.navigateToRoute(context, navigator, [SettingsScreen.screenNavInfo.routeName])),
-    // NavigationDividerSmall(),
-    NavigationItem(
-        InfoScreen.screenNavInfo,
-        (context, navigator) =>
-            NavigationUtils.navigateToRoute(context, navigator, [InfoScreen.screenNavInfo.routeName])),
+    _settingsNavigationItem,
+    _infoNavigationItem,
     NavigationDividerLarge(),
-    NavigationItem(
-      const ScreenNavInfo('Logout', Icons.exit_to_app, '/logout'),
-      (context, navigator) => Globals.logout(context),
-    ),
+    _logoutNavigationItem,
   ];
   static final List<NavigationItem> navigationBarItems = [
-    NavigationItem(
-        OverviewScreen.screenNavInfo, (context, navigator) => NavigationUtils.navigateToRoute(context, navigator, [])),
-    NavigationItem(
-        OperatingScreen.screenNavInfo,
-        (context, navigator) =>
-            NavigationUtils.navigateToRoute(context, navigator, [OperatingScreen.screenNavInfo.routeName])),
-    NavigationItem(
-        SolarPowerScreen.screenNavInfo,
-        (context, navigator) =>
-            NavigationUtils.navigateToRoute(context, navigator, [SolarPowerScreen.screenNavInfo.routeName])),
-    NavigationItem(
-        CarScreen.screenNavInfo,
-        (context, navigator) =>
-            NavigationUtils.navigateToRoute(context, navigator, [CarScreen.screenNavInfo.routeName])),
-    NavigationItem(
-        HeartScreen.screenNavInfo,
-        (context, navigator) =>
-            NavigationUtils.navigateToRoute(context, navigator, [HeartScreen.screenNavInfo.routeName])),
+    _overviewNavigationItem,
+    _operatingNavigationItem,
+    _solarPowerNavigationItem,
+    _carNavigationItem,
+    _heartNavigationItem,
   ];
   static final List<NavigationItemBase> navigationBarMenuItems = [
-    NavigationItem(
-        SettingsScreen.screenNavInfo,
-        (context, navigator) =>
-            NavigationUtils.navigateToRoute(context, navigator, [SettingsScreen.screenNavInfo.routeName])),
-    NavigationItem(
-        InfoScreen.screenNavInfo,
-        (context, navigator) =>
-            NavigationUtils.navigateToRoute(context, navigator, [InfoScreen.screenNavInfo.routeName])),
+    _settingsNavigationItem,
+    _infoNavigationItem,
     NavigationDividerSmall(),
-    NavigationItem(
-      const ScreenNavInfo('Logout', Icons.exit_to_app, '/logout'),
-      (context, navigator) => Globals.logout(context),
-    ),
+    _logoutNavigationItem,
   ];
 }
