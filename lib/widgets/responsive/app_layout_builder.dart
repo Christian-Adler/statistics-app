5deee3edcdb18c08f5724178596e8c5b08502b1f
Christian Adler <chris.adler@gmx.de>
2023-05-13 16:49:07 +0200

    add app layout builder

---
+++ lib/widgets/responsive/app_layout_builder.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_commons/utils/media_query_utils.dart';
+import 'package:provider/provider.dart';
+import 'package:statistics/providers/dynamic_theme_data.dart';
+
+import '../../providers/app_layout.dart';
+import '../navigation/navigation_menu_vertical.dart';
+
+class AppLayoutBuilder extends StatelessWidget {
+  final Widget body;
+
+  // Builder-Funktion, damit nur erzeugt wird, falls auch benoetigt.
+  final Widget Function()? bottomNavigationBarBuilder;
+
+  const AppLayoutBuilder({
+    Key? key,
+    required this.body,
+    this.bottomNavigationBarBuilder,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final appLayout = Provider.of<AppLayout>(context);
+    bool showNavigationTitle = appLayout.showNavigationItemTitle;
+
+    final mediaQueryInfo = MediaQueryUtils(MediaQuery.of(context));
+
+    Widget? bottomNavBarW;
+    if (mediaQueryInfo.isPortrait) {
+      final bottomNavigationBarBuilder = this.bottomNavigationBarBuilder;
+      if (bottomNavigationBarBuilder != null) {
+        bottomNavBarW = bottomNavigationBarBuilder();
+      }
+    }
+    final dynamicThemeData = Provider.of<DynamicThemeData>(context, listen: false);
+    dynamicThemeData.usePageTransition = !mediaQueryInfo.isTablet;
+
+    Widget? bodyW;
+    if (mediaQueryInfo.isLandscape && mediaQueryInfo.isTablet) {
+      bodyW = Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: showNavigationTitle ? 304 : 56,
+            child: Scaffold(
+              appBar: AppBar(),
+              body: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(1.0, 0.0), //(x,y)
+                      blurRadius: 6.0,
+                    ),
+                  ],
+                ),
+                child: NavigationMenuVertical(showNavigationTitle),
+              ),
+            ),
+          ),
+          Expanded(child: body),
+        ],
+      );
+    }
+    bodyW ??= body;
+
+    var colorScheme = Theme.of(context).colorScheme;
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      bottomNavigationBar: bottomNavBarW,
+      body: Container(
+        decoration: BoxDecoration(gradient: LinearGradient(colors: [colorScheme.primary, colorScheme.secondary])),
+        child: SafeArea(child: bodyW),
+      ),
+    );
+  }
+}
