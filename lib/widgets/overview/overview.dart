b86ec4aeb3afd67e0152bcb069bd66bab0eb46bc
Christian Adler <chris.adler@gmx.de>
2023-05-29 06:49:17 +0200

    display sensor test in overview

--- lib/widgets/overview/overview.dart
+++ lib/widgets/overview/overview.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_commons/utils/media_query_utils.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 import '../../utils/globals.dart';
 import '../layout/single_child_scroll_view_with_scrollbar.dart';
@@ -10,34 +14,208 @@ class Overview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.end,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Expanded(
-          child: Stack(
+          child: OverviewBody(),
+        ),
+        OverviewFooter(),
+      ],
+    );
+  }
+}
+
+class OverviewBody extends StatelessWidget {
+  const OverviewBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQueryInfo = MediaQueryUtils.of(context);
+    if (mediaQueryInfo.isTablet && mediaQueryInfo.isLandscape) {
+      return Parallax();
+    }
+
+    return Stack(
+      children: [
+        SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              SizedBox(
-                width: double.infinity,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      Globals.assetImgBackground,
-                      fit: BoxFit.cover,
-                    ),
-                  ],
-                ),
-              ),
-              const SingleChildScrollViewWithScrollbar(
-                // No BottomNavBar hide on Overview Screen // scrollDirectionCallback: HideBottomNavigationBar.setScrollDirection,
-                child: Center(child: OverviewNavigationButtons()),
+              Image.asset(
+                Globals.assetImgBackground,
+                fit: BoxFit.cover,
               ),
             ],
           ),
         ),
-        const OverviewFooter(),
+        const SingleChildScrollViewWithScrollbar(
+          // No BottomNavBar hide on Overview Screen // scrollDirectionCallback: HideBottomNavigationBar.setScrollDirection,
+          child: Center(child: OverviewNavigationButtons()),
+        ),
+      ],
+    );
+  }
+}
+
+class Parallax extends StatefulWidget {
+  const Parallax({
+    super.key,
+  });
+
+  @override
+  State<Parallax> createState() => _ParallaxState();
+}
+
+class _ParallaxState extends State<Parallax> {
+  List<double>? _userAccelerometerValues;
+  List<double>? _accelerometerValues;
+  List<double>? _gyroscopeValues;
+  List<double>? _magnetometerValues;
+  final _streamSubscriptions = <StreamSubscription<dynamic>>[];
+
+  @override
+  void dispose() {
+    super.dispose();
+    for (final subscription in _streamSubscriptions) {
+      subscription.cancel();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _streamSubscriptions.add(
+      userAccelerometerEvents.listen(
+        (UserAccelerometerEvent event) {
+          setState(() {
+            _userAccelerometerValues = <double>[event.x, event.y, event.z];
+          });
+        },
+        onError: (e) {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return const AlertDialog(
+                  title: Text("Sensor Not Found"),
+                  content: Text("It seems that your device doesn't support Accelerometer Sensor"),
+                );
+              });
+        },
+        cancelOnError: true,
+      ),
+    );
+    _streamSubscriptions.add(
+      accelerometerEvents.listen(
+        (AccelerometerEvent event) {
+          setState(() {
+            _accelerometerValues = <double>[event.x, event.y, event.z];
+          });
+        },
+        onError: (e) {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return const AlertDialog(
+                  title: Text("Sensor Not Found"),
+                  content: Text("It seems that your device doesn't support Gyroscope Sensor"),
+                );
+              });
+        },
+        cancelOnError: true,
+      ),
+    );
+    _streamSubscriptions.add(
+      gyroscopeEvents.listen(
+        (GyroscopeEvent event) {
+          setState(() {
+            _gyroscopeValues = <double>[event.x, event.y, event.z];
+          });
+        },
+        onError: (e) {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return const AlertDialog(
+                  title: Text("Sensor Not Found"),
+                  content: Text("It seems that your device doesn't support User Accelerometer Sensor"),
+                );
+              });
+        },
+        cancelOnError: true,
+      ),
+    );
+    _streamSubscriptions.add(
+      magnetometerEvents.listen(
+        (MagnetometerEvent event) {
+          setState(() {
+            _magnetometerValues = <double>[event.x, event.y, event.z];
+          });
+        },
+        onError: (e) {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return const AlertDialog(
+                  title: Text("Sensor Not Found"),
+                  content: Text("It seems that your device doesn't support Magnetometer Sensor"),
+                );
+              });
+        },
+        cancelOnError: true,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final userAccelerometer = _userAccelerometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
+    final accelerometer = _accelerometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
+    final gyroscope = _gyroscopeValues?.map((double v) => v.toStringAsFixed(1)).toList();
+    final magnetometer = _magnetometerValues?.map((double v) => v.toStringAsFixed(1)).toList();
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text('UserAccelerometer: $userAccelerometer'),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text('Accelerometer: $accelerometer'),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text('Gyroscope: $gyroscope'),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text('Magnetometer: $magnetometer'),
+            ],
+          ),
+        ),
       ],
     );
   }
