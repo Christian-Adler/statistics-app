fc1cd6568ca035b9af823fdb28346ed120cefb24
Christian Adler <chris.adler@gmx.de>
2023-06-10 06:12:56 +0200

    use normalized log level output

--- lib/utils/logging/log_utils.dart
+++ lib/utils/logging/log_utils.dart
@@ -46,31 +46,50 @@ class _Filter extends LogFilter {
 
 class _LogOutput extends LogOutput {
   @override
-  void output(OutputEvent event) async {
+  void output(OutputEvent event) {
     // FileOutput
-    String logMsg = '';
-    for (var line in event.lines) {
-      if (logMsg.isNotEmpty) logMsg += '\n';
-      logMsg += line;
-    }
-    logMsg = '${event.level.name} - $logMsg';
-    DailyFiles.writeToFile(logMsg);
+    _Outputs.outFile(event);
   }
 }
 
 class _LogOutputWithConsole extends LogOutput {
   @override
-  void output(OutputEvent event) async {
+  void output(OutputEvent event) {
+    _Outputs.outCons(event);
     // FileOutput
+    _Outputs.outFile(event);
+  }
+}
+
+class _Outputs {
+  static final Map<Level, String> _levelMapping = {
+    Level.verbose: 'VERB ',
+    Level.debug: 'DEBUG',
+    Level.info: 'INFO ',
+    Level.warning: 'WARN ',
+    Level.error: 'ERROR',
+    Level.wtf: 'WTF  ',
+  };
+
+  static void outFile(OutputEvent event) {
     String logMsg = '';
     for (var line in event.lines) {
-      if (kDebugMode) {
-        print('${event.level.name.toUpperCase()} $line');
-      }
       if (logMsg.isNotEmpty) logMsg += '\n';
       logMsg += line;
     }
-    logMsg = '${event.level.name.toUpperCase()} $logMsg';
+    logMsg = '${_normalizedLevel(event.level)} | $logMsg';
     DailyFiles.writeToFile(logMsg);
+  }
+
+  static void outCons(OutputEvent event) {
+    for (var line in event.lines) {
+      if (kDebugMode) {
+        print('${_normalizedLevel(event.level)} | $line');
+      }
+    }
+  }
+
+  static String _normalizedLevel(Level level) {
+    return _levelMapping[level] ?? 'UNKNOWN';
   }
 }
